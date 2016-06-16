5f8ea0e6bd476cfd349416876dc11f0a1d05e167
Yang Jiang <jiangyang5157@gmail.com>
2016-06-16 19:14:11 +1200

    fs

--- test/fs.go
+++ test/fs.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"github.com/ant0ine/go-json-rest/rest"
+)
+
+/*
+curl -i http://127.0.0.1:8080/api/message
+
+curl -i http://127.0.0.1:8080/static/
+curl -i http://127.0.0.1:8080/static/asd.txt
+*/
+
 func main() {
 
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+
+	router, err := rest.MakeRouter(
+		rest.Get("/message", func(w rest.ResponseWriter, req *rest.Request) {
+			w.WriteJson(map[string]string{"Body": "Hello World!"})
+		}),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.SetApp(router)
+
+	http.Handle("/api/", http.StripPrefix("/api", api.MakeHandler()))
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./assets"))))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
