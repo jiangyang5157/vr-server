5cbb7c074d18af55cf925dff07851b158473196e
Yang Jiang <jiangyang5157@gmail.com>
2016-06-25 19:40:22 +1200

    NA

--- server/main.go
+++ server/main.go
@@ -16,26 +16,25 @@ curl -i http://127.0.0.1:8080/assets/static/kml/example.kml
 
 func main() {
 
+	//rest server
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
-
 	router, err := rest.MakeRouter(
-		rest.Get("/test", test),
+		rest.Get("/test", func(w rest.ResponseWriter, r *rest.Request) {
+			w.WriteJson(map[string]string{
+				"key": "value",
+			})
+		}),
 	)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 	api.SetApp(router)
 
+	// file server
 	http.Handle("/api/", http.StripPrefix("/api", api.MakeHandler()))
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
 
 	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(app.PORT), nil))
 }
 
-func test(w rest.ResponseWriter, r *rest.Request) {
-	w.WriteJson(map[string]string{
-		"key": "value",
-	})
-}
