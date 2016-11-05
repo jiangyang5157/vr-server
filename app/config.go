22a00d5d03a6d48f20b8b4d387c851b6c19fa722
Yang Jiang <jiangyang5157@gmail.com>
2016-11-05 13:09:33 +1300

    update ip and port

---
+++ app/config.go
@@ -0,0 +1,4 @@
+package app
+
+const IP_ADDRESS = "192.168.1.51"
+const PORT int = 5157
--- server/main.go
+++ server/main.go
@@ -9,13 +9,13 @@ import (
 )
 
 /*
-curl -i http://127.0.0.1:8080/api/test
-curl -i http://192.168.1.67:8080/api/test
+IP Address and Port see app/config.go
+curl -i http://192.168.1.51:5157/api/test
 
-curl -i http://127.0.0.1:8080/assets/
-curl -i http://127.0.0.1:8080/assets/static/
-curl -i http://127.0.0.1:8080/assets/static.zip
-curl -i http://127.0.0.1:8080/assets/static/layer/example.kml
+curl -i http://127.0.0.1:5157/assets/
+curl -i http://127.0.0.1:5157/assets/static/
+curl -i http://127.0.0.1:5157/assets/static.zip
+curl -i http://127.0.0.1:5157/assets/static/layer/example.kml
 */
 
 func main() {
